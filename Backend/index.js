e9cc9f24ee4c7b9a6e8ca82a91ee9902c79ee0c0
Prashant Jain <54273112+pj263@users.noreply.github.com>
2021-03-19 22:00:38 -0400

    Delete index.js

--- Backend/index.js
+++
@@ -1,8 +0,0 @@
-exports.handler = async (event) => {
-    // TODO implement
-    const response = {
-        statusCode: 200,
-        body: { "username": "pj263" },
-    };
-    return response.body;
-};
