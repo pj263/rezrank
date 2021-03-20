6b92ac8fc1032e2720a6f5bc62e7c25ec96390d5
Prashant Jain <54273112+pj263@users.noreply.github.com>
2021-03-19 22:02:04 -0400

    added backend project
    https://9ixxufr9re.execute-api.us-east-1.amazonaws.com/whoami

---
+++ Backend/index.js
@@ -0,0 +1,8 @@
+exports.handler = async (event) => {
+    // TODO implement
+    const response = {
+        statusCode: 200,
+        body: { "username": "pj263" },
+    };
+    return response.body;
+};
